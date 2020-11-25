e83d2cc11cda06f2ddb08232986c98235afd44b3
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-25 14:32:28 +0700

    fix savew data to chrome storage

--- auto-check-user/public/js/content.js
+++ auto-check-user/public/js/content.js
@@ -25,21 +25,24 @@ $(document).ready(function () {
 
     $("#load").click(function () {
         // console.log($('#mssv_user2').val());
-        
+
         var get_input_mssv;
-        
+
         var total_user_mssv = $('#total_user_mssv').val();
         console.log(total_user_mssv);
 
+        //show data storage : https://chrome.google.com/webstore/detail/storage-area-explorer/ocfjjjjhkpapocigimmppepjgfdecjkb
         //save list mssv from server file to chrome storage
         chrome.storage.sync.get(["list"], function (result) {
             for (var i = 1; i <= total_user_mssv; i++) {
-                get_input_mssv = $('#mssv_user' + i).val();
-                var get_list = result['list'];
-                get_list['mssv'].push(get_input_mssv);
-                chrome.storage.sync.set({
-                    "list": get_list
-                });
+                if ($('#inlineCheckbox' + i).is(":checked") == true) {
+                    get_input_mssv = $('#mssv_user' + i).val();
+                    var get_list = result['list'];
+                    get_list['mssv'].push(get_input_mssv);
+                    chrome.storage.sync.set({
+                        "list": get_list
+                    });
+                }
             }
         });
 
