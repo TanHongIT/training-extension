bf160a7ef44c4f8cbd332ea4d91366d37940393a
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-29 22:20:23 +0700

    Test mssv users post up and auto-fill tabs

--- auto-check-user/public/js/content.js
+++ auto-check-user/public/js/content.js
@@ -17,8 +17,11 @@ function load_presence() {
 
 	chrome.storage.sync.get(["list_online"], function (result) {
 
-		//save list mssv from "main server online presence" to chrome storage
-		for (var i = 0; i <= 100; i++) {
+		//Attribute Starts With Selector: https://api.jquery.com/attribute-starts-with-selector/
+		var total_user_server = $('[id^="grvListStudents_lblStudentID_"]').length;
+
+		/*save list mssv from "main server online presence" to chrome storage*/
+		for (var i = 0; i < total_user_server; i++) {
 			if ($('#grvListStudents_lblStudentID_' + i)) {
 				get_mssv_server = $('#grvListStudents_lblStudentID_' + i).html();
 				var get_list = result['list_online'];
@@ -28,7 +31,24 @@ function load_presence() {
 				});
 			} else;
 		}
-		var list_user_server = result['list_online'];
 
+		var list_user_server = result['list_online'];
+		console.log("so user tren server: " + total_user_server + ", " + list_user_server['mssv_user'].length);
+
+		/* Test mssv users post up and auto-fill tabs */
+		chrome.storage.sync.get(["list"], function (result_sync) {
+			var list_user_presenced = result_sync['list'];
+			console.log(list_user_presenced['mssv']);
+			console.log(list_user_server['mssv_user']);
+
+			for (let i = 0; i < list_user_server['mssv_user'].length; i++) {
+				if (jQuery.inArray(list_user_server['mssv_user'][i], list_user_presenced['mssv']) !== -1) {
+					$('#grvListStudents_txtVangCP_' + i).val(0);
+				} else {
+					$('#inputnumber' + i).val(5);
+					$('#grvListStudents_txtVangKP_' + i).val(5);
+				}
+			}
+		});
 	});
 }
