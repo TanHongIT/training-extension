a5f0aa58030cf4a20a8af8ff261392bf3c3fab8a
tanhongit <phuongtan12357nguyen@gmail.com>
2020-11-29 20:13:11 +0700

    save list mssv from 'main server online presence' to chrome storage

--- auto-check-user/public/js/content.js
+++ auto-check-user/public/js/content.js
@@ -6,15 +6,29 @@ $(document).ready(function () {
 		sendResqonse
 	) {
 		if (request.message === "start") {
-			// chrome.storage.sync.get(["list"], function (result) {
-			// 	let total_user_presene = result['list'].length;
-			// 	console.log(total_user_presene);
-			// 	$('input[type=text]').val(result["userselected"]);
-			// 	$('button[type=submit]').click();
-			// });
-			chrome.storage.sync.get(["list"], function (result) {
-				console.log(result['list']);
-			});
+			load_presence();
 		}
 	});
 });
+
+function load_presence() {
+
+	var get_mssv_server;
+
+	chrome.storage.sync.get(["list_online"], function (result) {
+
+		//save list mssv from "main server online presence" to chrome storage
+		for (var i = 0; i <= 100; i++) {
+			if ($('#grvListStudents_lblStudentID_' + i)) {
+				get_mssv_server = $('#grvListStudents_lblStudentID_' + i).html();
+				var get_list = result['list_online'];
+				get_list['mssv_user'].push(get_mssv_server);
+				chrome.storage.sync.set({
+					"list_online": get_list
+				});
+			} else;
+		}
+		var list_user_server = result['list_online'];
+
+	});
+}
